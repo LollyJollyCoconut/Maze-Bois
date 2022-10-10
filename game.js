3044c0db52b738d118b6a2179db1c8e4ef2f0267
LollyJollyCoconut <danielpeng929@gmail.com>
2022-10-10 06:35:30 -0700

    e
    e

---
+++ game.js
@@ -0,0 +1,45 @@
+// let angleX;
+// function setup() {
+//   createCanvas(windowWidth, windowHeight, WEBGL);
+//   angleX = 0;
+// }
+
+// function draw() {
+//   background(255, 0, 0)
+//   rectMode(CENTER);
+//   angleX += 1;
+//   rotateX(radians(angleX));
+//   rotateY(radians(45));
+//   rotateZ(radians(45));
+//   box(50, 50, 100);
+// }
+let cellWidth = 20;
+let numOfCols, numOfRows;
+let cellsArray = [];
+function setup() {
+  createCanvas(width, height);
+  numOfCols = floor(width / cellWidth);
+  numOfRows = floor(height / cellWidth);
+  for(let rowNum = 0; rowNum < numOfRows; rowNum++) {
+    for (let colNum = 0; colNum < numOfCols;colNum++) {
+      let cell = new Cell(colNum, rowNum);
+      cellsArray.push(cell);
+      console.log(cell);
+    }
+  }
+}
+function draw() {
+  background (255,215,0);
+}
+class Cell {
+  constructor(cellRowNum, cellColNum){
+    this.rowNum = cellRowNum;
+    this.colNum = cellColNum;
+  }
+  show() {
+    let xPos = this.rowNum * cellWidth;
+    let yPos = this.colNum * cellWidth;
+    stroke (255);
+    line(xPos, yPos, xPos + cellWidth, yPos);
+  }
+}
