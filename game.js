3d15b8d6b43f1b0d9172d46e79054f699c9587ce
LollyJollyCoconut <danielpeng929@gmail.com>
2022-10-17 06:33:02 -0700

    e
    e

--- game.js
+++ game.js
@@ -17,7 +17,7 @@ let cellWidth = 20;
 let numOfCols, numOfRows;
 let cellsArray = [];
 function setup() {
-  createCanvas(width, height);
+  createCanvas(windowWidth, windowHeight);
   numOfCols = floor(width / cellWidth);
   numOfRows = floor(height / cellWidth);
   for(let rowNum = 0; rowNum < numOfRows; rowNum++) {
@@ -30,16 +30,42 @@ function setup() {
 }
 function draw() {
   background (255,215,0);
+  for (let cellNum = 0; cellNum < cellsArray.length; cellNum++) {
+    cellsArray[cellNum].show();
+  }
+}
+function getIndex(rowNum, colNum) {
+  return colNum + rowNum * numOfCols;
 }
 class Cell {
   constructor(cellRowNum, cellColNum){
     this.rowNum = cellRowNum;
     this.colNum = cellColNum;
+    this.walls = [true, true, true, true];
+    this.visited = false;
+  }
+  checkNeighbors() {
+    let neighbors = [];
+    let top = cellsArray[getIndex(this.rowNum, this.colNum - 1)];
+    let right = cellsArray[getIndex(this.rowNum + 1 , this.colNum)];
+    let bottom = cellsArray[getIndex(this.rowNum, this.colNum - 1)];
+    let left = cellsArray[getIndex(this.rowNum - 1, this.colNum)];
   }
   show() {
     let xPos = this.rowNum * cellWidth;
     let yPos = this.colNum * cellWidth;
     stroke (255);
-    line(xPos, yPos, xPos + cellWidth, yPos);
+    if (walls[0] == true) {
+      line(xPos, yPos, xPos + cellWidth, yPos);
+    }
+    if (walls[2] == true) {
+      line(xPos, yPos + cellWidth, xPos + cellWidth, yPos + cellWidth);
+    }
+    if (walls[3] == true) {
+      line(xPos, yPos, xPos, yPos + cellWidth);
+    }
+    if (walls[1] == true) {
+      line(xPos + cellWidth, yPos, xPos + cellWidth, yPos + cellWidth);
+    }
   }
 }
