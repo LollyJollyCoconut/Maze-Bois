917a1557b2d802d25a14894997181bbedc83ef38
LollyJollyCoconut <danielpeng929@gmail.com>
2022-10-24 06:30:57 -0700

    e
    e

--- game.js
+++ game.js
@@ -16,6 +16,7 @@
 let cellWidth = 20;
 let numOfCols, numOfRows;
 let cellsArray = [];
+let currentCellBeingVisited;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   numOfCols = floor(width / cellWidth);
@@ -27,44 +28,101 @@ function setup() {
       console.log(cell);
     }
   }
+  currentCellBeingVisited = cellsArray[0];
 }
 function draw() {
   background (255,215,0);
   for (let cellNum = 0; cellNum < cellsArray.length; cellNum++) {
     cellsArray[cellNum].show();
   }
+  currentCellBeingVisited.visited = true;
+  currentCellBeingVisited.highlight();
+  let nextCellToVisit = currentCellBeingVisited.checkNeighbors();
+  nextCellToVisit.highlight();
+  if (nextCellToVisit) {
+    nextCellToVisit.visited = true;
+    removeWalls(currentCellBeingVisited, nextCellToVisit);
+    currenctCellBeingVisited = nextCellToVisit;
+  }
 }
-function getIndex(rowNum, colNum) {
-  return colNum + rowNum * numOfCols;
+function getIndex(colNum, rowNum) {
+  if (colNum < 0 || rowNum < 0 || colNum > numOfCols - 1 || rowNum > numOfRows - 1) {
+     return -1;
+  }else {
+    return colNum + rowNum * numOfCols;
+  }
+}
+function removeWalls(cellA, cellB) {
+  let horizontalDifference = cellA.rowNum - cellB.rowNum;
+  if (horizontalDifference === 1) {
+    cellA.walls[3] = false;
+    cellB.walls[1] = false;
+  }else if (horizontalDifference === -1) {
+    cellA.walls[1] = false;
+    cellB.walls[3] = false;
+  }
+  let verticalDifference = cellA.colNum - cellB.colNum;
+  if (verticalDifference == 1) {
+    cellA.walls[0] = false;
+    cellB.walls[2] = false;
+  }else if (verticalDifference === -1) {
+    cellA.walls[2] = false;
+    cellB.walls[0] = false;
+  }
 }
 class Cell {
-  constructor(cellRowNum, cellColNum){
+  constructor(cellColNum, cellRowNum){
     this.rowNum = cellRowNum;
     this.colNum = cellColNum;
     this.walls = [true, true, true, true];
     this.visited = false;
   }
+  highlight() {
+    let xPos = this.colNum*cellWidth;
+    let yPos = this.rowNum*cellWidth;
+    noStroke();
+    fill(0, 0, 255, 100);
+    rect(xPos, yPos, cellWidth, cellWidth);
+  }
   checkNeighbors() {
     let neighbors = [];
-    let top = cellsArray[getIndex(this.rowNum, this.colNum - 1)];
-    let right = cellsArray[getIndex(this.rowNum + 1 , this.colNum)];
-    let bottom = cellsArray[getIndex(this.rowNum, this.colNum - 1)];
-    let left = cellsArray[getIndex(this.rowNum - 1, this.colNum)];
+    let top = cellsArray[getIndex(this.colNum, this.rowNum - 1)];
+    let right = cellsArray[getIndex(this.colNum + 1 , this.rowNum)];
+    let bottom = cellsArray[getIndex(this.colNum, this.rowNum - 1)];
+    let left = cellsArray[getIndex(this.colNum - 1, this.rowNum)];
+    if (top && top.visited == false) {
+      neighbors.push(top);
+    }
+    if (right && right.visited == false) {
+      neighbors.push(right);
+    }
+    if (bottom && bottom.visited == false) {
+      neighbors.push(bottom);
+    }
+    if (left && left.visited == false) {
+      neighbors.push(left);
+    }
+    if (neighbors.length > 0) {
+      let randomNeighborIndex = floor(random(0, neighbors.length));
+      return neighbors[randomNeighborIndex];
+    }else {
+      return undefined;
+    }
   }
   show() {
-    let xPos = this.rowNum * cellWidth;
-    let yPos = this.colNum * cellWidth;
+    let xPos = this.colNum * cellWidth;
+    let yPos = this.rowNum * cellWidth;
     stroke (255);
-    if (walls[0] == true) {
+    if (this.walls[0] == true) {
       line(xPos, yPos, xPos + cellWidth, yPos);
     }
-    if (walls[2] == true) {
+    if (this.walls[2] == true) {
       line(xPos, yPos + cellWidth, xPos + cellWidth, yPos + cellWidth);
     }
-    if (walls[3] == true) {
+    if (this.walls[3] == true) {
       line(xPos, yPos, xPos, yPos + cellWidth);
     }
-    if (walls[1] == true) {
+    if (this.walls[1] == true) {
       line(xPos + cellWidth, yPos, xPos + cellWidth, yPos + cellWidth);
     }
   }
