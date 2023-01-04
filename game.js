b254316ecf9ec8dbb03f9093bf8d8ee3c009c75a
LollyJollyCoconut <danielpeng929@gmail.com>
2023-01-03 22:04:03 -0800

    e
    e

--- game.js
+++ game.js
@@ -1,130 +1,5 @@
-tsParticles.load("tsparticles", {
-  "fullScreen": {
-    "zIndex": 1
-  },
-  "particles": {
-    "number": {
-      "value": 0
-    },
-    "color": {
-      "value": [
-        "#00FFFC",
-        "#FC00FF",
-        "#fffc00"
-      ]
-    },
-    "shape": {
-      "type": "circle",
-      "options": {}
-    },
-    "opacity": {
-      "value": 1,
-      "animation": {
-        "enable": true,
-        "minimumValue": 0,
-        "speed": 2,
-        "startValue": "max",
-        "destroy": "min"
-      }
-    },
-    "size": {
-      "value": 4,
-      "random": {
-        "enable": true,
-        "minimumValue": 2
-      }
-    },
-    "links": {
-      "enable": false
-    },
-    "life": {
-      "duration": {
-        "sync": true,
-        "value": 5
-      },
-      "count": 1
-    },
-    "move": {
-      "enable": true,
-      "gravity": {
-        "enable": true,
-        "acceleration": 10
-      },
-      "speed": {
-        "min": 10,
-        "max": 20
-      },
-      "decay": 0.1,
-      "direction": "none",
-      "straight": false,
-      "outModes": {
-        "default": "destroy",
-        "top": "none"
-      }
-    },
-    "rotate": {
-      "value": {
-        "min": 0,
-        "max": 360
-      },
-      "direction": "random",
-      "move": true,
-      "animation": {
-        "enable": true,
-        "speed": 60
-      }
-    },
-    "tilt": {
-      "direction": "random",
-      "enable": true,
-      "move": true,
-      "value": {
-        "min": 0,
-        "max": 360
-      },
-      "animation": {
-        "enable": true,
-        "speed": 60
-      }
-    },
-    "roll": {
-      "darken": {
-        "enable": true,
-        "value": 25
-      },
-      "enable": true,
-      "speed": {
-        "min": 15,
-        "max": 25
-      }
-    },
-    "wobble": {
-      "distance": 30,
-      "enable": true,
-      "move": true,
-      "speed": {
-        "min": -15,
-        "max": 15
-      }
-    }
-  },
-  "emitters": {
-    "life": {
-      "count": 0,
-      "duration": 0.1,
-      "delay": 0.4
-    },
-    "rate": {
-      "delay": 0.1,
-      "quantity": 150
-    },
-    "size": {
-      "width": 0,
-      "height": 0
-    }
-  }
-});
 const particles = tsParticles.domItem(0);
+let playerObject;
 let cellWidth = 100;
 let wallWidth = 50;
 let wallHeight = 50;
@@ -138,9 +13,11 @@ let cameraXPos = 0;
 let cameraYPos = 0;
 let cameraZPos = 1000;
 let cameraIncrement = 50;
+let player;
 
 function preload(){
   wallTexture = loadImage("E.png");
+  playerObject = loadModel('Baller.obj');
 }
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -148,15 +25,16 @@ function setup() {
   numOfRows = floor(height / cellWidth);
   for(let rowNum = 0; rowNum < numOfRows; rowNum++) {
     for (let colNum = 0; colNum < numOfCols;colNum++) {
-      let cell = new Cell(colNum, rowNum);
+      let cell = new Cell(colNum, rowNum); 
       cellsArray.push(cell);
     }
   }
   currentCellBeingVisited = cellsArray[0];
-  particles.play();
+  player = new Player();
 }
 function draw() {
   background (255,215,0);
+  lights();
   modifyCameraSettings();
   // frustum(-1000000, 1000000, -1000000, 1000000, -1000000, 1000000);
   camera(cameraXPos, cameraYPos, cameraZPos, 0, 0, 0, 0, 1, 0);
@@ -176,6 +54,7 @@ function draw() {
   else if (stack.length > 0){
     currentCellBeingVisited = stack.pop();
   }
+  player.show();
 }
 function modifyCameraSettings() {
   if (keyIsDown(87)) {
@@ -299,5 +178,22 @@ class Cell {
       fill(60, 279, 113, 100);
       rect(xPos, yPos, cellWidth, cellWidth);
     }
+  }
+}
+class Player {
+  constructor() {
+    this.rowNum = 0;
+    this.colNum = 0;
+    this.modelObject = playerObject;
+  }
+  show() {
+    let xPos = this.colNum*cellWidth + cellWidth/2;
+    let yPos = this.rowNum*cellWidth + cellWidth/2;
+    let zPos = wallHeight/2;
+    translate(xPos, yPos, zPos);
+    scale(0.5);
+    normalMaterial();
+    model(this.modelObject);
+    translate(-1*(xPos), -1*(yPos), -1*(zPos));
   }
 }
